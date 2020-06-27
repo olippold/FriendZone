57bdd04d88f1a994c1aa03ae95181ec290c4db05
Oliver Lippold <olippold@hotmail.com>
2020-06-27 15:05:53 +0100

    Initial Commit

---
+++ FriendZone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FriendZone
+//
+//  Created by Oliver Lippold on 27/06/2020.
+//  Copyright © 2020 Oliver Lippold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
