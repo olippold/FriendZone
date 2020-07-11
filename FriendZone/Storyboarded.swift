90631db907636b7d3e61ef6219f9c4988d5424a9
Oliver Lippold <olippold@hotmail.com>
2020-07-11 13:22:48 +0100

    Added new branch

---
+++ FriendZone/Storyboarded.swift
@@ -0,0 +1,23 @@
+//
+//  Storyboarded.swift
+//  FriendZone
+//
+//  Created by Oliver Lippold on 04/07/2020.
+//  Copyright © 2020 Oliver Lippold. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol Storyboarded {
+    static func instantiate() -> Self
+}
+
+extension Storyboarded where Self: UIViewController {
+    
+    static func instantiate() -> Self {
+        let className = String(describing: self)
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        return storyboard.instantiateViewController(withIdentifier: className) as! Self
+    }
+}
